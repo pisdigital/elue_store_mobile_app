203499f39adc0882e0671abe4c3e2d99f11b6d0f
pisdigital <pisdigital.dev@gmail.com>
2025-06-20 17:49:11 +0300

    account delete

--- lib/features/personalization/screens/setting/settings.dart
+++ lib/features/personalization/screens/setting/settings.dart
@@ -69,12 +69,6 @@ class SettingsScreen extends StatelessWidget {
                       showActionButton: false,
                     ),
                     const SizedBox(height: TSizes.spaceBtwItems),
-                    // TSettingsMenuTile(
-                    //   icon: Iconsax.safe_home,
-                    //   title: 'My Addresses',
-                    //   subTitle: 'Set shopping delivery address',
-                    //   onTap: () => Get.to(() => const UserAddressScreen()),
-                    // ),
                     TSettingsMenuTile(
                       icon: Iconsax.shopping_cart,
                       title: 'My Cart',
@@ -87,71 +81,37 @@ class SettingsScreen extends StatelessWidget {
                       subTitle: 'In-progress and Completed Orders',
                       onTap: () => Get.to(() => const OrderScreen()),
                     ),
-                    // const TSettingsMenuTile(
-                    //     icon: Iconsax.bank, title: 'Bank Account', subTitle: 'Withdraw balance to registered bank account'),
-                    // const TSettingsMenuTile(
-                    //     icon: Iconsax.discount_shape, title: 'My Coupons', subTitle: 'List of all the discounted coupons'),
-                    // TSettingsMenuTile(
-                    //     icon: Iconsax.notification, title: 'Notifications', subTitle: 'Set any kind of notification message', onTap: () {}),
-                    // const TSettingsMenuTile(
-                    //     icon: Iconsax.security_card, title: 'Account Privacy', subTitle: 'Manage data usage and connected accounts'),
-
-                    /// -- App Settings
-                    // const SizedBox(height: TSizes.spaceBtwSections),
-                    // const TSectionHeading(title: 'App Settings', showActionButton: false),
-                    // const SizedBox(height: TSizes.spaceBtwItems),
-                    // TSettingsMenuTile(
-                    //   icon: Iconsax.document_upload,
-                    //   title: 'Load Data',
-                    //   subTitle: 'Upload Data to your Cloud Firebase',
-                    //   onTap: () => Get.to(() => const UploadDataScreen()),
-                    // ),
-                    // const SizedBox(height: TSizes.spaceBtwItems),
-                    // TSettingsMenuTile(
-                    //   icon: Iconsax.location,
-                    //   title: 'Geolocation',
-                    //   subTitle: 'Set recommendation based on location',
-                    //   trailing: Switch(value: true, onChanged: (value) {}),
-                    // ),
-                    // TSettingsMenuTile(
-                    //   icon: Iconsax.security_user,
-                    //   title: 'Safe Mode',
-                    //   subTitle: 'Search result is safe for all ages',
-                    //   trailing: Switch(value: false, onChanged: (value) {}),
-                    // ),
-                    // TSettingsMenuTile(
-                    //   icon: Iconsax.image,
-                    //   title: 'HD Image Quality',
-                    //   subTitle: 'Set image quality to be seen',
-                    //   trailing: Switch(value: false, onChanged: (value) {}),
-                    // ),
                     const SizedBox(height: TSizes.spaceBtwSections),
 
-                    const Center(
-                      child: 
-                      InkWell( () {
-                            controller.deleteUserAccount(); 
-                                    },
-                       child: Text('Delete Account', style: Theme.of(context).textTheme.bodySmall))))
+                    
 
                     /// -- Logout Button
                     const SizedBox(height: TSizes.spaceBtwSections),
-
                     SizedBox(
                       width: double.infinity,
                       child: OutlinedButton(
-                        onPressed:
-                            () =>
-                                controller.isUserLoggedIn()
-                                    ? controller.logout()
-                                    : Get.offAll(const LoginScreen()),
-                        child:
-                            controller.isUserLoggedIn()
-                                ? const Text('Logout')
-                                : const Text("Login"),
+                        onPressed: () => controller.isUserLoggedIn()
+                            ? controller.logout()
+                            : Get.offAll(const LoginScreen()),
+                        child: controller.isUserLoggedIn()
+                            ? const Text('Logout')
+                            : const Text("Login"),
                       ),
                     ),
+
                     const SizedBox(height: TSizes.spaceBtwSections * 2.5),
+
+                    Center(
+  child: InkWell(
+    onTap: () => controller.deleteUserAccount(),
+    child: Text(
+      'Delete Account',
+      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+        color: Colors.red, // This sets the text color to red
+      ),
+    ),
+  ),
+),
                   ],
                 ),
               ),
