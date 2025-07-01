605c161fafeab78d2ba260cad1dbdd4b6104fa70
Yoko Hailemariam <yokohailemariam@gmail.com>
2025-07-01 14:40:45 +0300

    refactor: comment out unused imports and code in HomeScreen

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,16 +1,16 @@
-import 'package:elue_store/common/widgets/custom_shapes/containers/secondary_header_container.dart';
+// import 'package:elue_store/common/widgets/custom_shapes/containers/secondary_header_container.dart';
 import 'package:elue_store/features/shop/controllers/home_controller.dart';
 import 'package:elue_store/features/shop/controllers/product/product_controller.dart';
 import 'package:elue_store/features/shop/screens/home/widgets/header_detail_container.dart';
 import 'package:elue_store/features/shop/screens/home/widgets/information_services_widget.dart';
-import 'package:elue_store/features/shop/screens/home/widgets/promo_slider.dart';
-import 'package:elue_store/features/shop/screens/services/contact_info.dart';
-import 'package:elue_store/features/shop/screens/services/courses.dart';
-import 'package:elue_store/features/shop/screens/services/design.dart';
-import 'package:elue_store/features/shop/screens/services/flight_booking.dart';
-import 'package:elue_store/features/shop/screens/services/printing.dart';
-import 'package:elue_store/features/shop/screens/services/socials.dart';
-import 'package:elue_store/utils/constants/colors.dart';
+// import 'package:elue_store/features/shop/screens/home/widgets/promo_slider.dart';
+// import 'package:elue_store/features/shop/screens/services/contact_info.dart';
+// import 'package:elue_store/features/shop/screens/services/courses.dart';
+// import 'package:elue_store/features/shop/screens/services/design.dart';
+// import 'package:elue_store/features/shop/screens/services/flight_booking.dart';
+// import 'package:elue_store/features/shop/screens/services/printing.dart';
+// import 'package:elue_store/features/shop/screens/services/socials.dart';
+// import 'package:elue_store/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -35,80 +35,67 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // Retrieve existing instances of controllers
     final controller = Get.put(ProductController());
-    final homeController = Get.put(HomeController());
+    // final homeController = Get.put(HomeController());
 
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             Obx(() {
-              if (homeController.activeTabIndex == 1) {
-                /// Header
-                return SecondaryHeaderContainer(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    /// -- Appbar
-                    const THomeAppBar(),
-                    const SizedBox(height: TSizes.spaceBtwSections),
+              // if (homeController.activeTabIndex == 1) {
+              //   /// Header
+              //   return const SecondaryHeaderContainer(
+              //       child: Column(
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     children: [
+              //       /// -- Appbar
+              //       THomeAppBar(),
+              //       SizedBox(height: TSizes.spaceBtwSections),
 
-                    const HeaderDetailContainer(),
-                    const SizedBox(height: TSizes.spaceBtwSections),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TabChanger(
-                            controller: homeController,
-                            index: 0,
-                            title: "Products"),
-                        const SizedBox(width: 15),
-                        TabChanger(
-                            controller: homeController,
-                            index: 1,
-                            title: "Services"),
-                      ],
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwSections),
-                  ],
-                ));
-              }
+              //       HeaderDetailContainer(),
+              //       SizedBox(height: TSizes.spaceBtwSections),
+              //       // Row(
+              //       //   mainAxisAlignment: MainAxisAlignment.center,
+              //       //   children: [
+              //       //     TabChanger(
+              //       //         controller: homeController,
+              //       //         index: 0,
+              //       //         title: "Products"),
+              //       //     const SizedBox(width: 15),
+              //       //     TabChanger(
+              //       //         controller: homeController,
+              //       //         index: 1,
+              //       //         title: "Services"),
+              //       //   ],
+              //       // ),
+              //       SizedBox(height: TSizes.spaceBtwSections),
+              //     ],
+              //   ));
+              // }
 
               return
 
                   /// Header
-                  TPrimaryHeaderContainer(
+                  const TPrimaryHeaderContainer(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     /// -- Appbar
-                    const THomeAppBar(),
-                    const SizedBox(height: TSizes.spaceBtwSections),
+                    THomeAppBar(),
+                    SizedBox(height: TSizes.spaceBtwSections),
 
-                    const HeaderDetailContainer(),
-                    const SizedBox(height: TSizes.spaceBtwSections),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TabChanger(
-                            controller: homeController,
-                            index: 0,
-                            title: "Products"),
-                        const SizedBox(width: 15),
-                        TabChanger(
-                            controller: homeController,
-                            index: 1,
-                            title: "Services"),
-                      ],
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwSections),
+                    HeaderDetailContainer(),
+                    SizedBox(height: TSizes.spaceBtwSections),
+
+                    SizedBox(height: TSizes.spaceBtwSections),
 
                     /// -- Searchbar
-                    const TSearchContainer(text: 'Search', showBorder: true),
-                    const SizedBox(height: TSizes.spaceBtwSections),
+                    TSearchContainer(text: 'Search', showBorder: true),
+                    SizedBox(height: TSizes.spaceBtwSections),
 
                     /// -- Categories
-                    const THeaderCategories(),
-                    const SizedBox(height: TSizes.spaceBtwSections),
+                    THeaderCategories(),
+                    SizedBox(height: TSizes.spaceBtwSections),
                   ],
                 ),
               );
@@ -117,9 +104,9 @@ class HomeScreen extends StatelessWidget {
             /// -- Body
             Obx(
               () {
-                if (homeController.activeTabIndex == 1) {
-                  return InformationServicesWidget();
-                }
+                // if (homeController.activeTabIndex == 1) {
+                //   return InformationServicesWidget();
+                // }
 
                 // Default content for other activeTabIndex values
                 return Container(
